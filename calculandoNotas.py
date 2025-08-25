697b98f96d9860f5c52b96e71cacf696d80f6bf3
Sandro Souza <isxndro@hotmail.com>
2025-08-25 11:20:38 -0300

    update

--- calculandoNotas.py
+++ calculandoNotas.py
@@ -1,17 +1,17 @@
-import math
+import math #importa a biblioteca matemática, mas não é usada nesse exemplo. 
 
-print('Média de 2 alunos:')
-n1 = 8
-n2 = 9
-#calculando a média:
-mediaNotas = (n1 + n2) / 2 
+print('Média de 2 alunos:') #imprime o título na tela 
+n1 = 8 #nota do aluno 1
+n2 = 9 #nota do aluno 2
+#calculando a média: 
+mediaNotas = (n1 + n2) / 2 #faz a soma das notas e divide por 2 
 
 #imprime o resultado:
-print('A média das notas é:', mediaNotas) 
+print('A média das notas é:', mediaNotas) #imprime o resultado na tela
 
-#nota de 4 alunos
+#nota de 4 alunos 
 
-print('Média de 4 alunos:')
+print('Média de 4 alunos:') 
 n1 = 8
 n2 = 9
 n3 = 10
@@ -20,5 +20,5 @@ n4 = 5
 #calculando a média:
 media = (n1+n2+n3+n4) / 4
 
-#imprime o resultado:
+#imprime o resultado: 
 print('A média é de: ', media)
--- contarPalavras.py
+++ contarPalavras.py
@@ -2,9 +2,10 @@
 
 #inserção de dados
 usuario = input("Qual seu nome: ")
-frase = input(f"Olá {usuario}! por favor, insira a frase desejada: ")
-palavras = frase.split()
-total_pavras = len(palavras)
-#lógica do programa
-print(f"Olá {usuario}! o total de pavras é: {total_pavras}.")
+frase = input(f"Olá {usuario}! por favor, insira a frase desejada: ") #peça uma frase ao usuário. 
+palavras = frase.split() #split() separa a string em uma lista de palavras.
+total_pavras = len(palavras) #len() conta a quantidade de elementos em uma lista. palavras é uma lista.
+#imprime o resultado
+print(f"{usuario}, o total de pavras é: {total_pavras}!")
+
 
